658918098314d975698478ee6a98594387de9aba
itskannan2002 <thalavk008@gmail.com>
2022-03-27 21:40:09 +0530

    this is my first commit

---
+++ greatkart/views.py
@@ -0,0 +1,12 @@
+from itertools import product
+from django.shortcuts import render
+from store.models import Product
+from store.models import Product
+
+def home(request):
+    products = Product.objects.all().filter(is_available=True)
+
+    context = { 
+        'products': products,
+    }
+    return render(request,'home.html', context)
