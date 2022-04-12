47cc957bcf5485108609f0e79e83ffc3d566152c
itskannan2002 <thalavk008@gmail.com>
2022-04-12 22:20:14 +0530

    handled cart and checkout page for the logged in users

--- accounts/views.py
+++ accounts/views.py
@@ -20,6 +20,14 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 
 
+from carts.views import _cart_id
+from carts.models import Cart,CartItem
+import requests
+
+
+
+
+
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -30,15 +38,14 @@ def register(request):
             phone_number = form.cleaned_data['phone_number']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-            username = email.split('@')[0]
-
-            user = Account.objects.create_user(first_name = first_name, last_name = last_name,email= email, username = username, password = password)
+            username = email.split("@")[0]
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
 
-            # user activation 
+            # USER ACTIVATION
             current_site = get_current_site(request)
-            mail_subject = "please activate your account "
+            mail_subject = 'Please activate your account'
             message = render_to_string('accounts/account_verification_email.html', {
                 'user': user,
                 'domain': current_site,
@@ -48,15 +55,14 @@ def register(request):
             to_email = email
             send_email = EmailMessage(mail_subject, message, to=[to_email])
             send_email.send()
-            #messages.success(request, 'Thank you for registering with us. we have sent you a verification email to your email address. please verify it.')
+            # messages.success(request, 'Thank you for registering with us. We have sent you a verification email to your email address [rathan.kumar@gmail.com]. Please verify it.')
             return redirect('/accounts/login/?command=verification&email='+email)
     else:
         form = RegistrationForm()
-    context ={
-        'form' : form,
-        
+    context = {
+        'form': form,
     }
-    return render(request, 'accounts/register.html',context)
+    return render(request, 'accounts/register.html', context)
 
 def login(request):
     if request.method == 'POST':
@@ -66,15 +72,62 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+                    # Getting the product variations by cart id
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    # Get the cart items from the user to access his product variations
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    # product_variation = [1, 2, 3, 4, 6]
+                    # ex_var_list = [4, 6, 3, 5]
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+            except:
+                pass
             auth.login(request, user)
-            messages.success(request, 'you are login in now')
-
-            return redirect('dashboard')
-
+            messages.success(request, 'You are now logged in.')
+            url = request.META.get('HTTP_REFERER')    
+            try:
+                query = requests.utils.urlparse(url).query
+                # next=/cart/checkout/
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)                
+            except:
+                return redirect('dashboard')
         else:
-            messages.error(request, 'Invalid credentials')
+            messages.error(request, 'Invalid login credentials')
             return redirect('login')
     return render(request, 'accounts/login.html')
+
 
 
 @login_required(login_url = 'login')
--- carts/context_processors.py
+++ carts/context_processors.py
@@ -6,15 +6,15 @@ def counter(request):
     cart_count = 0
     if 'admin' in request.path:
         return {}
-    else: 
+    else:
         try:
             cart = Cart.objects.filter(cart_id=_cart_id(request))
-            cart_items = CartItem.objects.all().filter(cart=cart[:1])
+            if request.user.is_authenticated:
+                cart_items = CartItem.objects.all().filter(user=request.user)
+            else:
+                cart_items = CartItem.objects.all().filter(cart=cart[:1])
             for cart_item in cart_items:
                 cart_count += cart_item.quantity
-
         except Cart.DoesNotExist:
             cart_count = 0
-    return dict(cart_count = cart_count)
-
-
+    return dict(cart_count=cart_count)
